93e2ab46abefdbd769b432ef9cf4c87b9a3bddc3
AmeyPank <pankantiameya@gmail.com>
2023-06-13 16:12:28 +0530

    Icon in title and logo added to

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import AccountIcon from "./AccountCircle";
+
 // import CompareButton from './CompareButton'
+
 import LOGO from "../logo.png";
 const Header = () => {
   return (
     <div className="header">
       <div className="logo">
-        <img src={LOGO} alt="" /></div>
+        <img src={LOGO} alt="" />
+      </div>
       <div className="user-logo" style={{ marginTop: "15px" }}>
         <AccountIcon />
       </div>
--- src/Context/TestModeContext.jsx
+++ src/Context/TestModeContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext, useState } from "react";
 const TestModeContext = createContext();
 
 export const TestModeContextProvider = ({ children }) => {
-  const [testTime, setTestTime] = useState(5);
+  const [testTime, setTestTime] = useState(15);
   // const [testSeconds, setTestSeconds] = useState(15);
   // const [testWords, setTestWords] = useState(10); // 10 or 20 or 30
 

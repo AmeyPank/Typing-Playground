029726322dcd4901c4ed17dad956b9983130b709
AmeyPank <pankantiameya@gmail.com>
2023-06-17 04:01:31 +0530

    point is not registerded

--- src/Components/Graph.jsx
+++ src/Components/Graph.jsx
@@ -1,102 +1,63 @@
 import React from "react";
-import { Line } from "react-chartjs-2";
-import { useTheme } from "../Context/ThemeContext";
 import {
-  Chart,
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
   Title,
   Tooltip,
   Legend,
-  CategoryScale,
-  LinearScale,
 } from "chart.js";
 
-Chart.register(Title, Tooltip, Legend, CategoryScale, LinearScale);
+import { useTheme } from "../Context/ThemeContext";
+import { Line } from "react-chartjs-2";
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 function Graph({ graphData }) {
   const { theme } = useTheme();
-
-  // Get the recent 20 days of data
-  const recentData = graphData.slice(-20);
-
-  // Get the start and end date
-  const startDate = graphData[0][0];
-  const endDate = graphData[graphData.length - 1][0];
-
+  console.log(graphData);
   return (
     <>
-      {/* Recent 20 days graph */}
-      <div>
-        <h2>Recent 20 Days</h2>
-        <Line
-          data={{
-            labels: recentData.map((i) => i[0]),
-            datasets: [
-              {
-                type: "line",
-                data: recentData.map((i) => i[1]),
-                label: "wpm",
-                borderColor: theme.title,
-              },
-            ],
-          }}
-          options={{
-            scales: {
-              x: {
+      <Line
+        data={{
+          labels: graphData.map((i) => i[0]),
+          datasets: [
+            {
+              type: "line",
+              data: graphData.map((i) => i[1]),
+              label: "wpm",
+              borderColor: theme.title,
+            },
+          ],
+        }}
+        options={{
+          scales: {
+            x: {
+              display: true,
+              title: {
                 display: true,
-                title: {
-                  display: true,
-                  text: "Time in Seconds",
-                },
-              },
-              y: {
-                display: true,
-                title: {
-                  display: true,
-                  text: "Words per minute",
-                },
+                text: "Time in Seconds",
               },
             },
-          }}
-        />
-      </div>
-
-      {/* All data graph with start and end dates */}
-      <div>
-        <h2>
-          All Data (Start: {startDate}, End: {endDate})
-        </h2>
-        <Line
-          data={{
-            labels: graphData.map((i) => i[0]),
-            datasets: [
-              {
-                type: "line",
-                data: graphData.map((i) => i[1]),
-                label: "wpm",
-                borderColor: theme.title,
-              },
-            ],
-          }}
-          options={{
-            scales: {
-              x: {
+            y: {
+              display: true,
+              title: {
                 display: true,
-                title: {
-                  display: true,
-                  text: "Time in Seconds",
-                },
-              },
-              y: {
-                display: true,
-                title: {
-                  display: true,
-                  text: "Words per minute",
-                },
+                text: "Words per minute",
               },
             },
-          }}
-        />
-      </div>
+          },
+        }}
+      />
     </>
   );
 }
